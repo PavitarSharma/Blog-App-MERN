a2d7449c3be36f402af27ac8d21936efc8328d5d
PavitarSharma <pavitarsharma144@gmail.com>
2023-03-10 23:26:33 +0530

    create backend and api for blog

---
+++ backend/app.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const helmet = require("helmet");
+const bodyParser = require("body-parser");
+const connectDB = require("./utils/db");
+const path = require("path")
+require("dotenv").config();
+const userRoutes = require("./routes/user.routes")
+const commentRoutes = require("./routes/comment.routes")
+const postsRoutes = require("./routes/posts.routes")
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+// aplly middewares
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(helmet());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+// routes
+app.use("/api/v1/users", userRoutes)
+app.use("/api/v1/posts", postsRoutes)
+app.use("/api/v1/comments", commentRoutes)
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Server connection established"
+  })
+})
+
+app.use("*", (req, res, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
+
+app.use(async (err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send({
+    error: {
+      status: err.status || 500,
+      message: err.message,
+    },
+  });
+});
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGODB_URL);
+    app.listen(PORT, console.log(`Server is listening on port ${PORT}... `));
+  } catch (error) {
+    console.log(error.message)
+  }
+};
+
+start()
